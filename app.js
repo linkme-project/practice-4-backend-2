9e61147284750ae15411b9c94c97a7637b63f383
Keunhak Lim <limkeunhak@gmail.com>
2019-06-03 09:19:56 +0900

    미들웨어 위치 변경

--- app.js
+++ app.js
@@ -18,10 +18,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/items', itemsRouter);
 app.use('/', (req, res) => {
   res.status(200).json('hello backend-2');
 });
-app.use('/items', itemsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
