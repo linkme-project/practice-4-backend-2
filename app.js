2728e1f1f1c5be33ea76224ca852422e276771db
Keunhak Lim <limkeunhak@gmail.com>
2019-06-03 09:16:18 +0900

    "/" 경로 추가

--- app.js
+++ app.js
@@ -18,6 +18,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/', (req, res) => {
+  res.status(200).json('hello backend-2');
+});
 app.use('/items', itemsRouter);
 
 // catch 404 and forward to error handler
